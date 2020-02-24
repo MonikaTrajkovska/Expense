979a7b0ff012b8cc68dbb979059536338af6f2b8
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2020-02-24 16:10:38 +0100

    adding message

---
+++ client/build/precache-manifest.1f31c8973727f884a94cb267aa8e5786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8337033f315295147dc72b6578df6f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e77134785984e8585f",
+    "url": "/static/css/main.06a7a7c3.chunk.css"
+  },
+  {
+    "revision": "0d425ca5fd765c0b7313",
+    "url": "/static/js/2.6b669875.chunk.js"
+  },
+  {
+    "revision": "ec879bfcc6abcedf81d6091c498a5563",
+    "url": "/static/js/2.6b669875.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7e77134785984e8585f",
+    "url": "/static/js/main.9d2a7285.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "4b4b47a0436265ef31d0687e78b50cfe",
+    "url": "/static/media/Cost-Calculator.4b4b47a0.jpg"
+  }
+]);

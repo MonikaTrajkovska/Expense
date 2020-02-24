afee8fdf6cce494037a70cb748ef34cb008238c8
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2020-02-24 23:15:29 +0100

    adding message

--- services/client.js
+++ services/client.js
@@ -6,12 +6,19 @@ const path = require('path');
 const api = express();
 api.use(bodyParser.json());
 api.use(cors());
+api.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
+});
 
 api.use(express.static(path.join(__dirname, '../client/build')));
  //api.get('*', (req, res) => {
  // res.sendFile(path.resolve(__dirname + '/client/build/index.html'));
  //});
-api.get('/*', function (req, res) {
+api.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 

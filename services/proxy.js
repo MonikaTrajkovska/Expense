275e4246a7cdb8765275133708836622923de576
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2020-02-25 17:27:56 +0100

    message added

--- services/proxy.js
+++ services/proxy.js
@@ -26,7 +26,7 @@ api.all('/api/v1/auth/*', (req, res) => {
 //     res.status(404).send('Not Found!');
 // });
 
- api.listen(process.env.PORT, err => {
+ api.listen(process.env.PORT || 8082, err => {
      if(err){
         console.log('could not start server');
          console.log(err);

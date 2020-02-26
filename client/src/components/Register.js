df5c7c90c56fa2a4786b386521f66f71f04a97d8
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2020-02-26 13:39:44 +0100

    message added

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -51,7 +51,7 @@ import './Register.css'
              this.state.password != null
              ) {
              event.preventDefault()
-             axios.post('https://mern-app-expensecalculator.herokuapp.com/api/v1/auth/register', {
+             axios.post('/api/v1/auth/register', {
                  first_name: this.state.first_name,
                  last_name: this.state.last_name,
                  email: this.state.email,
@@ -63,7 +63,7 @@ import './Register.css'
              })
              .then(res => {
                  console.log(res)
-                 axios.post('https://mern-app-expensecalculator.herokuapp.com/api/v1/auth/login', {
+                 axios.post('/api/v1/auth/login', {
                      email: this.state.email,
                      password: this.state.password
                  })
